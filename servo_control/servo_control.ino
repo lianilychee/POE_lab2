f9e0f16cf0c081ff6309bfdc1f2c8bb3aaf3d3f8
lianilychee <lye.liani@gmail.com>
2014-09-19 10:36:31 -0400

    successfully printing to serial monitor

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -7,31 +7,30 @@
 // is called the “duty cycle”. Most hobby servos use a pulse width of between 1ms and 2ms
 // (sometimes the range can be as wide as 0.8ms to 2.2ms).
 
+
 // Sweep
 // by BARRAGAN <http://barraganstudio.com> 
 // This example code is in the public domain.
 
-
 #include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
+Servo myservo;  // create servo object to control a servo (maximum of eight servo objects can be created)
 int pos = 0;    // variable to store the servo position 
  
 void setup() { 
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  Serial.begin(9600);
 } 
  
- 
 void loop() { 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
+  for(pos = 0; pos < 180; pos += 1){  // goes from 0 degrees to 180 degrees // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
+    Serial.println(pos);
+    delay(15);      // waits 15ms for the servo to reach the position 
+  }
+  for(pos = 180; pos>=1; pos-=1){     // goes from 180 degrees to 0 degrees 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    Serial.println(pos);
     delay(15);                       // waits 15ms for the servo to reach the position 
   } 
 }
