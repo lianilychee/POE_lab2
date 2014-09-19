121cc5f94b79b1d142763e90ad33235871c2862a
lianilychee <lye.liani@gmail.com>
2014-09-19 11:51:29 -0400

    eliminated extraneous things

--- analoginoutserial/analoginoutserial.ino
+++ analoginoutserial/analoginoutserial.ino
@@ -1,22 +1,5 @@
 /*
-  Analog input, analog output, serial output
- 
- Reads an analog input pin, maps the result to a range from 0 to 255
- and uses the result to set the pulsewidth modulation (PWM) of an output pin.
- Also prints the results to the serial monitor.
- 
- The circuit:
- * potentiometer connected to analog pin 0.
-   Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
- 
- created 29 Dec. 2008
- modified 9 Apr 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- 
+Test: verify that sensor voltage/distance readings match datasheet 
  */
 
 // These constants won't change.  They're used to give names
@@ -37,14 +20,13 @@ void loop() {
   // change the analog out value:
   analogWrite(analogOutPin, outputValue);           
 
-  // print the results to the serial monitor:
-  Serial.print("sensor = " );                       
-  Serial.print(sensorValue);      
-  Serial.print("\t output = ");      
-  Serial.println(outputValue);   
+  Serial.println(sensorValue);
 
-  // wait 2 milliseconds before the next loop
-  // for the analog-to-digital converter to settle
-  // after the last reading:
-  delay(2);                     
+  // print the results to the serial monitor:
+//  Serial.print("sensor = " );                       
+//  Serial.print(sensorValue);      
+//  Serial.print("\t output = ");      
+//  Serial.println(outputValue);   
+
+  delay(2);   // wait 2ms before next loop for analog-digital converter to settle after last reading                  
 }
