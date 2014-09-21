648f92034e85dcd1153fb2e11e33eafd80684934
lianilychee <lye.liani@gmail.com>
2014-09-21 12:34:29 -0400

    comments added to script

--- irSensor_calibration.m
+++ irSensor_calibration.m
@@ -1,3 +1,7 @@
+% This script is for the IR Sensor calibration.
+% In order to scale the raw sensor reading to volts, divide the raw
+% sensor reading by 200.
+
 data = load('irSensor_calibration.csv');
 sensor_out = data(:,1);
 [length, columns] = size(sensor_out);
@@ -7,9 +11,12 @@ clipped_out = sensor_out(600:2500);
 [clipped_length, columns] = size(clipped_out);
 clipped_time = linspace(1,clipped_length,clipped_length);
 
+scaled_clipped = clipped_out/200;
+
 figure(1);
 clf;
 hold on;
 % plot(time, sensor_out, 'r');
-plot(clipped_time, clipped_out);
-axis([0 2000 0 700]);
+% plot(clipped_time, clipped_out);
+plot(clipped_time, scaled_clipped, 'r');
+% axis([0 2000 0 700]);
