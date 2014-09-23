51be14d572f3e49df0cfc4c2277f81d28a28c8d6
lianilychee <lye.liani@gmail.com>
2014-09-23 11:47:29 -0400

    overall file created

---
+++ all/all.ino
@@ -0,0 +1,22 @@
+/*
+Test: verify that sensor voltage/distance readings match datasheet 
+ */
+
+// Pin definition
+const float analogInPin = A0;  // Infrared sensor to Analog
+float sensorVal = 0;        // value read from the infrared sensor
+float v = 0;    // voltage output
+float dist = 0;    // distance in cm
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop() {
+  sensorVal = analogRead(analogInPin);    // read analog in value        
+  v = sensorVal/200;   // convert to volts
+  
+  Serial.println(sensorVal);
+
+  delay(2);   // wait 2ms before next loop for analog-digital converter to settle after last reading                  
+}
