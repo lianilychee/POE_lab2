16f847c6f15d3e3e9e5b9efd7d2a94b134a4e746
lianilychee <lye.liani@gmail.com>
2014-09-26 11:03:59 -0400

    so tilt suddenly works...

--- sensor_calibration/servo_control/servo_control.ino
+++ sensor_calibration/servo_control/servo_control.ino
@@ -1,15 +1,3 @@
-// Control the pan/tilt mechaism.
-// Record input for MATLAB analysis.
-
-// A hobby servo is controlled using a square wave form that is pulsed at a fixed frequency. 
-// The time that the waveform signal is set to “on” determines what the position of the shaft should be. 
-// This scheme is called “pulse width modulation” (PWM) and the percentage of “on” time during the period
-// is called the “duty cycle”. Most hobby servos use a pulse width of between 1ms and 2ms
-// (sometimes the range can be as wide as 0.8ms to 2.2ms).
-
-
-// Built off of sweep by BARRAGAN <http://barraganstudio.com> (public domain).
-
 #include <Servo.h> 
  
 // Initialize servos
@@ -26,31 +14,41 @@ void setup() {
  
 void loop() { 
   
-  for(pan_pos = 0; pan_pos < 180; pan_pos += 1){  // goes from 0 degrees to 180 degrees // in steps of 1 degree 
-    
-    pan.write(pan_pos);              // tell servo to go to position in variable 'pos' 
-    Serial.print("Pan: ");
-    Serial.println(pan_pos);
-    delay(15);      // waits 15ms for the servo to reach the position 
-    
-    tilt_pos = tilt_pos + 5;
+  for (tilt_pos = 0; tilt_pos < 180; tilt_pos += 1) {
     tilt.write(tilt_pos);
-    Serial.print("Tilt: ");
-    Serial.println(tilt_pos);
     delay(15);
   }
-  
-  for(pan_pos = 180; pan_pos >= 1; pan_pos -= 1){     // goes from 180 degrees to 0 degrees 
-  
-    pan.write(pan_pos);              // tell servo to go to position in variable 'pos' 
-    Serial.print("Pan: ");
-    Serial.println(pan_pos);
-    delay(15);                       // waits 15ms for the servo to reach the position 
-    
-    tilt_pos = tilt_pos + 5;
+  for (tilt_pos = 180; tilt_pos >= 1; tilt_pos -= 1) {
     tilt.write(tilt_pos);
-    Serial.print("Tilt: ");
-    Serial.println(tilt_pos);
     delay(15);
-  } 
+  }
+
+  
+//  for(pan_pos = 0; pan_pos < 180; pan_pos += 1){  // goes from 0 degrees to 180 degrees // in steps of 1 degree 
+//    
+//    pan.write(pan_pos);              // tell servo to go to position in variable 'pos' 
+//    Serial.print("Pan: ");
+//    Serial.println(pan_pos);
+//    delay(15);      // waits 15ms for the servo to reach the position 
+//    
+//    tilt_pos = tilt_pos + 5;
+//    tilt.write(tilt_pos);
+//    Serial.print("Tilt: ");
+//    Serial.println(tilt_pos);
+//    delay(15);
+//  }
+//  
+//  for(pan_pos = 180; pan_pos >= 1; pan_pos -= 1){     // goes from 180 degrees to 0 degrees 
+//  
+//    pan.write(pan_pos);              // tell servo to go to position in variable 'pos' 
+//    Serial.print("Pan: ");
+//    Serial.println(pan_pos);
+//    delay(15);                       // waits 15ms for the servo to reach the position 
+//    
+//    tilt_pos = tilt_pos + 5;
+//    tilt.write(tilt_pos);
+//    Serial.print("Tilt: ");
+//    Serial.println(tilt_pos);
+//    delay(15);
+//  } 
 }
