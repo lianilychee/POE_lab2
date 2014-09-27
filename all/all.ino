913a18d9f839943bd4c4ea067dafec4946369084
lianilychee <lye.liani@gmail.com>
2014-09-27 16:16:29 -0400

    ir integrated with motor code

--- all/all.ino
+++ all/all.ino
@@ -1,10 +1,16 @@
 #include <Servo.h> 
  
-// Initialize servos
+// Servo definition
 Servo pan;  // servo object to control a servo (maximum eight servo objects can be created)
 int pan_pos = 0;    // store servo position 
 Servo tilt;
 int tilt_pos = 0;
+
+// Pin definition
+const float analogInPin = A0;  // Infrared sensor to Analog
+float sensorVal = 0;        // value read from the infrared sensor
+float v = 0;    // voltage output
+float dist = 0;    // distance in cm
  
 void setup() { 
   pan.attach(9);  // attaches the servo on pin 9 to the servo object 
@@ -27,11 +33,15 @@ void loop() {
       delay(15);
     } 
     
+    sensorVal = analogRead(analogInPin);    // read analog in value        
+    v = sensorVal/200;   // convert to volts
+    delay(15);
+    
     Serial.print(tilt_pos);
     Serial.print(",");
     Serial.print(pan_pos);
     Serial.print(",");
-    Serial.println(1);
+    Serial.println(sensorVal);
   }
   
   for (pan_pos = 180; pan_pos >= 1; pan_pos -= 1){     // goes from 180 degrees to 0 degrees 
@@ -43,11 +53,16 @@ void loop() {
       tilt.write(tilt_pos);
       delay(15);
     }
+    
+    sensorVal = analogRead(analogInPin);    // read analog in value        
+    v = sensorVal/200;   // convert to volts
+    delay(15);
+    
     Serial.print(tilt_pos);
     Serial.print(",");
     Serial.print(pan_pos);
     Serial.print(",");
-    Serial.println(1);
+    Serial.println(sensorVal);
   }
  
   if (tilt_pos >= 180) {
