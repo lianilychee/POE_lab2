ae6223d66365d28fdb291ce8ef0729cd5045453a
lianilychee <lye.liani@gmail.com>
2014-09-27 19:15:04 -0400

    sensor reading to distance conversion made

--- data_analysis.m
+++ data_analysis.m
@@ -7,13 +7,27 @@
 % A = fscanf(obj);
 % plot( A(:,1), A(:,2) );
 
-data = load ('motor_motion.csv');
+data = load ('ir_distance.csv');
 pan = data(:,2);
 tilt = data(:,1);
 ir = data(:,3);
 
+% Creating fake time
+time = zeros(5,1);
+
+
 % Validating servo motion
-figure(1);
+% figure(1);
+% clf;
+% hold on;
+% plot (pan, tilt);
+
+% Validating ir distance reading
+figure(2);
 clf;
 hold on;
-plot (pan, tilt);
+plot (1:length(ir), ir);
+
+
+conversion1 = 515*ones(1,length(ir));
+d = -((ir-conversion1)/9);
