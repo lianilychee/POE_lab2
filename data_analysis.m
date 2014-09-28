9adeaed1c3c9ea6268832469beed40aec87d7c14
lianilychee <lye.liani@gmail.com>
2014-09-28 13:16:34 -0400

    adding curve compensation

--- data_analysis.m
+++ data_analysis.m
@@ -12,22 +12,56 @@ pan = data(:,2);
 tilt = data(:,1);
 ir = data(:,3);
 
-% Creating fake time
-time = zeros(5,1);
-
-
-% Validating servo motion
+% % Validating servo motion
 % figure(1);
 % clf;
 % hold on;
 % plot (pan, tilt);
 
-% Validating ir distance reading
-figure(2);
-clf;
-hold on;
-plot (1:length(ir), ir);
+% % Validating ir distance reading
+% figure(2);
+% clf;
+% hold on;
+% plot (1:length(dist), dist);
 
+% IR conversion
+conversion1 = 515*ones(length(ir),1);
+dist = -((ir-conversion1)/9); % Convert IR mV reading to cm
 
-conversion1 = 515*ones(1,length(ir));
-d = -((ir-conversion1)/9);
+% Tilt conversion
+y = zeros(length(tilt),1);
+for i = 1:length(tilt);
+    if tilt(i) == 90;
+        y(i) = 12.207;
+    end
+    if tilt(i) < 90;
+        y(i) = 12.207 - (12.207*sin(tilt(i)));
+    end
+    if tilt(i) > 90;
+        y(i) = 12.207 + (12.207*sin(tilt(i)));
+    end
+end
+
+% Pan conversion
+x = zeros(length(pan),1);
+for i = 1:5;
+    pan(i)
+    if pan(i) == 90;
+        x(i) = 0;
+        x(i)
+    end
+    if pan(i) > 90;
+        x(i) = sin(pan(i)) * cos(tilt(i)) * dist(i);
+        x(i)
+    end
+    if pan < 90;
+        x(i) = -sin(pan(i)) * cos(tilt(i)) * dist(i);
+        x(i)
+    end
+end
+
+% Plot the damn thing
+% plot3(x,y,dist);
+% xlabel('xlabel');
+% ylabel('ylabel');
+% zlabel('zlabel');
